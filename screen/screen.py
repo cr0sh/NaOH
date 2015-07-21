3d31bd9b402a2a43c9a03418e78ac90c523c5d46
scalar77 <ska827@naver.com>
2015-07-22 02:00:19 +0900

    Added an icon

--- screen/screen.py
+++ screen/screen.py
@@ -1,10 +1,14 @@
 from tkinter import Tk, Frame, ttk
+from os import path
 
 class Screen:
 	root = Tk()
 	frame = ttk.Frame()
 	def __init__(self):
 		self.frames = list()
+		print(str(Screen.root._w))
+		Screen.root.call('wm', 'iconbitmap', Screen.root._w, '-default', path.join(path.dirname(path.dirname(path.realpath(__file__))), 'icon.ico'))
+		Screen.root.iconbitmap(path.join(path.dirname(path.dirname(path.realpath(__file__))), 'icon.ico'))
 		Screen.frame.destroy()
 		Screen.frame=Frame(Screen.root)
 		Screen.frame.pack(expand='false', padx=5, pady=5, fill='both')
